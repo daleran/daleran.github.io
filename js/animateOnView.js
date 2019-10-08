0a5f0b78759dd78d734027f9852dd39f6567773b
Sean Davis <daleran@gmail.com>
2019-10-08 22:00:53 +0900

    update 3

--- js/animateOnView.js
+++ js/animateOnView.js
@@ -8,12 +8,33 @@ $.fn.isInViewport = function() {
   return elementBottom > viewportTop && elementTop < viewportBottom;
 };
 
+var scrollTimer, lastScrollFireTime = 0;
+
 $(window).on('resize scroll', function() {
-  $('.anim-trigger').each(function() {
-    const thisElem = $(this);
-    if (thisElem.isInViewport()) {
-      var anims = $(this).find('.anim-elem').addClass('triggered');
-      thisElem.removeClass('anim-trigger');
-    }
-  });
+  
+  var minScrollTime = 100;
+  var now = new Date().getTime();
+
+  function processScroll() {
+      console.log(new Date().getTime().toString());
+      $('.anim-trigger').each(function() {
+        const thisElem = $(this);
+        if (thisElem.isInViewport()) {
+          var anims = $(this).find('.anim-elem').addClass('triggered');
+          thisElem.removeClass('anim-trigger');
+        }
+      });
+  }
+
+  if (!scrollTimer) {
+      if (now - lastScrollFireTime > (3 * minScrollTime)) {
+          processScroll();   // fire immediately on first scroll
+          lastScrollFireTime = now;
+      }
+      scrollTimer = setTimeout(function() {
+          scrollTimer = null;
+          lastScrollFireTime = new Date().getTime();
+          processScroll();
+      }, minScrollTime);
+  }
 });
--- scripts.js
+++ scripts.js
@@ -8,15 +8,37 @@ $.fn.isInViewport = function () {
   return elementBottom > viewportTop && elementTop < viewportBottom;
 };
 
+var scrollTimer,
+    lastScrollFireTime = 0;
 $(window).on('resize scroll', function () {
-  $('.anim-trigger').each(function () {
-    var thisElem = $(this);
+  var minScrollTime = 100;
+  var now = new Date().getTime();
 
-    if (thisElem.isInViewport()) {
-      var anims = $(this).find('.anim-elem').addClass('triggered');
-      thisElem.removeClass('anim-trigger');
+  function processScroll() {
+    console.log(new Date().getTime().toString());
+    $('.anim-trigger').each(function () {
+      var thisElem = $(this);
+
+      if (thisElem.isInViewport()) {
+        var anims = $(this).find('.anim-elem').addClass('triggered');
+        thisElem.removeClass('anim-trigger');
+      }
+    });
+  }
+
+  if (!scrollTimer) {
+    if (now - lastScrollFireTime > 3 * minScrollTime) {
+      processScroll(); // fire immediately on first scroll
+
+      lastScrollFireTime = now;
     }
-  });
+
+    scrollTimer = setTimeout(function () {
+      scrollTimer = null;
+      lastScrollFireTime = new Date().getTime();
+      processScroll();
+    }, minScrollTime);
+  }
 });
 $(document).ready(function () {
   $('.fellow-container').slick({
